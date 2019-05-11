9954751fd19f42d76f1052bc04a9e395cda6138e
rogeralsing <rogeralsing@gmail.com>
2019-05-11 16:06:47 +0200

    intersect

--- src/ranges.ts
+++ src/ranges.ts
@@ -1,47 +1,54 @@
 export type Range = {
-  from: number;
-  to: number;
+    from: number;
+    to: number;
 };
 
-export function intersection(a: Range[], b: Range[], overlap: number = 0) {
-  a = union(a, overlap);
-  b = union(b, overlap);
-  const c: Range[] = [];
-
-  // a  ###   ###   #########
-  // b ###########        #####
-  //
-  // r  ###   ###         ###
-  for (const x of a) {
-    for (const y of b) {
-      if (
+export function intersect(x: Range, y: Range): Range | null {
+    if (
         (x.from <= y.from && x.to >= y.from) ||
         (y.from <= x.from && y.to >= x.from)
-      ) {
-        const intersection = {
-          from: Math.max(x.from, y.from),
-          to: Math.min(x.to, y.to)
+    ) {
+        return {
+            from: Math.max(x.from, y.from),
+            to: Math.min(x.to, y.to)
         };
-        c.push(intersection);
-      }
     }
-  }
+    return null;
+}
 
-  return c;
+export function intersection(a: Range[], b: Range[], overlap: number = 0) {
+    a = union(a, overlap);
+    b = union(b, overlap);
+    const c: Range[] = [];
+
+    // a  ###   ###   #########
+    // b ###########        #####
+    //
+    // r  ###   ###         ###
+    for (const x of a) {
+        for (const y of b) {
+            const intersection = intersect(x,y);
+            if (intersection){
+                c.push(intersection);
+            }
+        }
+    }
+
+    return c;
 }
 
 export function union(ranges: Range[], overlap: number = 0): Range[] {
-  const result: Range[] = [];
-  let last: Range | null = null;
+    const result: Range[] = [];
+    let last: Range | null = null;
 
-  for (const r of ranges) {
-    if (!last || r.from > last.to + overlap) {
-      // +overlap to merge spans next to eachother
-      result.push((last = r));
-    } else if (r.to > last.to) {
-      last.to = r.to;
+    for (const r of ranges) {
+        if (!last || r.from > last.to + overlap) {
+            // +overlap to merge spans next to each other
+            result.push((last = r));
+        } else if (r.to > last.to) {
+            last.to = r.to;
+        }
     }
-  }
 
-  return result;
+    return result;
 }
