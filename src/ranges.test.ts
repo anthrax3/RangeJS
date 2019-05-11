db01372111034e59ff1b62d15922f533bd53646a
rogeralsing <rogeralsing@gmail.com>
2019-05-11 14:58:19 +0200

    .

--- src/ranges.test.ts
+++ src/ranges.test.ts
@@ -1,8 +1,22 @@
 import * as ranges from "./ranges";
 
-test("basic", () => {
+test("Union two consecutive ranges results in a single range", () => {
   const r: ranges.Range[] = [{ from: 1, to: 2 }, { from: 2, to: 3 }];
-
   const res = ranges.union(r);
+
   expect(res.length === 1);
+  const first = res[0];
+  expect(first.from === 1);
+  expect(first.to === 3);
+});
+
+test("Intersecting two overlapping ranges results in a single range", () => {
+  const r1: ranges.Range[] = [{ from: 1, to: 4 }];
+  const r2: ranges.Range[] = [{ from: 2, to: 3 }];
+  const res = ranges.intersection(r1, r2);
+
+  expect(res.length === 1);
+  const first = res[0];
+  expect(first.from === 1);
+  expect(first.to === 3);
 });
