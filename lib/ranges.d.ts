f37d6300e550f241397ab58a52244dd5e3d4a015
rogeralsing <rogeralsing@gmail.com>
2019-05-11 20:51:48 +0200

    intersect

--- lib/ranges.d.ts
+++ lib/ranges.d.ts
@@ -2,5 +2,5 @@ export declare type Range = {
     from: number;
     to: number;
 };
-export declare function intersection(a: Range[] | Range, b: Range[], overlap?: number): Range[];
+export declare function intersection(a: Range[] | Range, b: Range[] | Range, overlap?: number): Range[];
 export declare function union(ranges: Range[], overlap?: number): Range[];
